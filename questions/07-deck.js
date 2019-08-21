6cb00f29183669eed35bfa3b4efbe50876c36de8
Samuel <samuel_singer@apple.com>
2019-08-20 18:25:08 -0700

    deck.js solution broken

--- questions/07-deck.js
+++ questions/07-deck.js
@@ -1,22 +1,63 @@
-const Shuffle = require('./06-shuffle');
-
-class DeckOfCards extends Shuffle {
+class DeckOfCards {
   constructor() {
-    super();
-
-    this.deck; // whatever you want for the data structure
+    this.deck = [];
     this.suits = ['hearts', 'diamonds', 'clubs', 'spades'];
     this.values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'ace'];
+    this.numberOfPlayers = 5;
   }
 
   // using the data provided (suits and values) construct a deck of 52 cards
-  createDeck() {}
+  createDeck() {
+    for (let suit of this.suits) {
+      for (let value of this.values) {
+        let newObject = {};
+        newObject[suit] = value;
+        this.deck.push(newObject);
+      }
+    }
+    // You would probably want to check for unique values, no duplicates
+    // What happens if you'd like to add Two Jokers?
+  }
 
   // feel free to use the shuffle methods that we defined previously
-  shuffle() {}
+  shuffle() {
+    for (let i = this.deck.length - 1; i > 0; i--) {
+      let randomIndex = Math.floor(Math.random() * i);
+
+      let temp = this.deck[i];
+      this.deck[i] = this.deck[randomIndex];
+      this.deck[randomIndex] = temp;
+    }
+  }
 
   // Deal five cards, when you've hit the end of the deck and there are less than 5 to deal return a notification
-  dealFive() {}
+  dealFive() {
+    // Create and object of players
+    let objectOfPlayers = {}
+    // Key reprsents their position
+    for (let i = 1; i <= this.numberOfPlayers; i++) {
+      objectOfPlayers[i] = []
+    }
+    
+    while (this.deck.length >= 6) {
+      for (let player in objectOfPlayers) {
+        let topCard = this.deck.pop();
+        objectOfPlayers[player].push(topCard);
+      }
+    }
+
+    console.log(`There are ${this.deck.length} cards left in the deck`)
+    return objectOfPlayers
+  }
 }
 
 module.exports = DeckOfCards;
+
+const deck1 = new DeckOfCards()
+console.log(deck1.deck)
+deck1.createDeck()
+console.log(deck1.deck)
+console.log(deck1.deck.length)
+deck1.shuffle()
+console.log(deck1.deck)
+deck1.dealFive()
